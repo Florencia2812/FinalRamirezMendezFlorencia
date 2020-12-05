904ea3dd9725f13664490560538904026e5b99a1
Florencia Ramirez <a2019110598@estudiantes.upsa.edu.bo>
2020-12-05 11:16:42 -0400

    Solución crada

---
+++ Operacion/Operacion/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Operacion.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Operacion/Operacion/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Operacion.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
